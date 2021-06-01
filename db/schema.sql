6b33c7e815deb17332df6dde0e5fca142c5fd7e5
natcalderon <nat.calderon04@gmail.com>
2021-06-01 17:15:36 -0600

    initial setup

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+);
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mysql = require('mysql2');
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '8016350968',
+      database: 'tracker'
+    },
+    console.log('Connected to the tracker database.')
+  );
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
